bf770d4ac9cc2afd827a687850786b587b2cc2c3
Tsuzu <8574909+tsuzu@users.noreply.github.com>
2023-02-06 00:38:56 +0900

    Fix?

--- .github/workflows/build-byoh.js
+++ .github/workflows/build-byoh.js
@@ -11,8 +11,8 @@ module.exports = async () => {
 
     await $`cd ${byohDir} && cat << EOF > Makefile
 host-agent-binaries-arm64: ## Builds the binaries for the host-agent
-    RELEASE_BINARY=./byoh-hostagent GOOS=linux GOARCH=arm64 GOLDFLAGS="$(LDFLAGS) $(STATIC)" \
-    HOST_AGENT_DIR=./$(HOST_AGENT_DIR) $(MAKE) host-agent-binary`
+    RELEASE_BINARY=./byoh-hostagent GOOS=linux GOARCH=arm64 GOLDFLAGS="\$(LDFLAGS) \$(STATIC)" \
+    HOST_AGENT_DIR=./\$(HOST_AGENT_DIR) \$(MAKE) host-agent-binary`
     await $`cd ${byohDir} && make host-agent-binaries-arm64`
 
     await $`ls byoh/_dist`
