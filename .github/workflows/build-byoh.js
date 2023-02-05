a36749dd2f2658cbe2f0c44fd1f7d3bffc4982b9
Tsuzu <8574909+tsuzu@users.noreply.github.com>
2023-02-06 00:34:53 +0900

    Build for arm64

--- .github/workflows/build-byoh.js
+++ .github/workflows/build-byoh.js
@@ -6,7 +6,14 @@ module.exports = async () => {
 
     await $`git clone https://github.com/vmware-tanzu/cluster-api-provider-bringyourownhost.git -b ${byohVersion} --depth=1 ${byohDir}`
 
+    await $`cp metadata.yaml ./${byohDir}/metadata.yaml`
     await $`cd ${byohDir} && IMG="ghcr.io/miscord-win/cluster-api-byoh-controller:${byohVersion}" make build-release-artifacts`
+
+    await $`cd ${byohDir} && cat << EOF > Makefile
+host-agent-binaries-arm64: ## Builds the binaries for the host-agent
+    RELEASE_BINARY=./byoh-hostagent GOOS=linux GOARCH=arm64 GOLDFLAGS="$(LDFLAGS) $(STATIC)" \
+    HOST_AGENT_DIR=./$(HOST_AGENT_DIR) $(MAKE) host-agent-binary`
+    await $`cd ${byohDir} && make host-agent-binaries-arm64`
 
     await $`ls byoh/_dist`
 }
