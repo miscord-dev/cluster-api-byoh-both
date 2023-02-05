79c1bb1759f8405fa57de3f1a866a12dfb7cbefe
Tsuzu <8574909+tsuzu@users.noreply.github.com>
2023-02-05 18:47:01 +0900

    Fix a bug

--- .github/workflows/build-byoh.js
+++ .github/workflows/build-byoh.js
@@ -1,12 +1,3 @@
-module.exports = ({
-    github,
-    context,
-    core,
-    glob,
-    io,
-    exec,
-    fetch,
-    require,
-}) => {
-    return context.payload.client_payload.value
+module.exports = () => {
+    return "Hello"
 }
