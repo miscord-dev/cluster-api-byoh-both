301bd77c5b3a7783f5865f24f06c348f8268f841
Tsuzu <8574909+tsuzu@users.noreply.github.com>
2023-02-06 00:45:59 +0900

    Fix?

--- .github/workflows/build-byoh.js
+++ .github/workflows/build-byoh.js
@@ -9,10 +9,10 @@ module.exports = async () => {
     await $`cp metadata.yaml ./${byohDir}/metadata.yaml`
     await $`cd ${byohDir} && IMG="ghcr.io/miscord-win/cluster-api-byoh-controller:${byohVersion}" make build-release-artifacts`
 
-    await $`cat << EOF >> ./byoh/Makefile
+    await $`cat << EOF >> ./${byohDir}/Makefile
 host-agent-binaries-arm64: ## Builds the binaries for the host-agent
-    RELEASE_BINARY=./byoh-hostagent GOOS=linux GOARCH=arm64 GOLDFLAGS="\$(LDFLAGS) \$(STATIC)" \
-    HOST_AGENT_DIR=./\$(HOST_AGENT_DIR) \$(MAKE) host-agent-binary`
+    RELEASE_BINARY=./byoh-hostagent GOOS=linux GOARCH=arm64 GOLDFLAGS="\\$(LDFLAGS) \\$(STATIC)" \\
+    HOST_AGENT_DIR=./\\$(HOST_AGENT_DIR) \\$(MAKE) host-agent-binary`
     await $`cd ${byohDir} && make host-agent-binaries-arm64`
 
     await $`ls byoh/_dist`
