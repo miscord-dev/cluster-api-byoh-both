38fa2fbdfb13db804422200eb12d79e0c20fe3c6
Tsuzu <8574909+tsuzu@users.noreply.github.com>
2023-02-05 19:43:40 +0900

    Fix

--- .github/workflows/build-byoh.js
+++ .github/workflows/build-byoh.js
@@ -1,3 +1,12 @@
-module.exports = () => {
-    return "Hello"
+module.exports = async () => {
+    const byohVersion = (await $`cat go.mod | grep github.com/vmware-tanzu/cluster-api-provider-bringyourownhost`)
+        .stdout
+        .match(/(v\d+\.\d+\.\d+)/)[0]
+    const byohDir = `./byoh`;
+
+    await $`git clone https://github.com/vmware-tanzu/cluster-api-provider-bringyourownhost.git -b ${byohVersion} --depth=1 ${byohDir}`
+
+    await $`cd ${byohDir} && IMG="ghcr.io/miscord-win/cluster-api-byoh-controller:${byohVersion}" make build-release-artifacts`
+
+    await $`ls byoh/_dist`
 }
